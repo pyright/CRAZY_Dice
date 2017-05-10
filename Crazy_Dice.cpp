e8e6a33602d4201032dc8585e22377a1589e53bc
pyright <camposmike92@gmail.com>
2017-05-10 15:17:59 -0500

    made inputs from cmd line const

--- Crazy_Dice.cpp
+++ Crazy_Dice.cpp
@@ -26,21 +26,21 @@ int main(int argc, char* argv[]) {
   }
   
 // check num_faces
-  int num_faces = atoi(argv[1]);
+  const int num_faces = atoi(argv[1]);
   if(num_faces < 0) { //Needs better checks. Check if !NULL
 	fprintf(stderr, "error: num_faces must be >= 0\n");
 	exit(-1);
   }
  
 // check dice_floor 
-  int dice_floor = atoi(argv[2]);
+  const int dice_floor = atoi(argv[2]);
   if (dice_floor < 0) {    //Need better checks. Check if !NULL
     fprintf(stderr, "error: dice_floor must be >= 0\n");
     exit(-1);
   }
   
  // check dice_roof
-  int dice_roof = atoi(argv[3]);
+  const int dice_roof = atoi(argv[3]);
   if (dice_roof < dice_floor) { //Need better checks. Check if !NULL
     fprintf(stderr, "error: dice_roof must be at least = dice_floor\n");
     exit(-1);
@@ -48,7 +48,7 @@ int main(int argc, char* argv[]) {
   
   
   // check num_rolls
-  int num_rolls = atoi(argv[4]);
+  const int num_rolls = atoi(argv[4]);
   if (num_rolls < 0) {    //Need better checks. Check if !NULL
 	  fprintf(stderr, "error: num_rolls must be >= 0\n");
 	  exit(-1);
