3b80c5c42eb3b5c2f374c96aff77e6714856236b
pyright <camposmike92@gmail.com>
2017-05-10 15:16:59 -0500

    Added 4th command line input for num_faces
    Now, user can input the number of dice faces. num_faces will be a const that can be used to created the dice_array.

--- Crazy_Dice.cpp
+++ Crazy_Dice.cpp
@@ -20,20 +20,27 @@
 int main(int argc, char* argv[]) {
 	
 // check command line
-  if (argc != 3) {
-    fprintf(stderr, "usage: %s dice_floor  dice_roof  num_rolls\n", argv[0]); //#######################
+  if (argc != 4) {
+    fprintf(stderr, "usage: %s num_faces dice_floor  dice_roof  num_rolls\n", argv[0]); //#######################
     exit(-1);
+  }
+  
+// check num_faces
+  int num_faces = atoi(argv[1]);
+  if(num_faces < 0) { //Needs better checks. Check if !NULL
+	fprintf(stderr, "error: num_faces must be >= 0\n");
+	exit(-1);
   }
  
 // check dice_floor 
-  int dice_floor = atoi(argv[1]);
+  int dice_floor = atoi(argv[2]);
   if (dice_floor < 0) {    //Need better checks. Check if !NULL
     fprintf(stderr, "error: dice_floor must be >= 0\n");
     exit(-1);
   }
   
  // check dice_roof
-  int dice_roof = atoi(argv[2]);
+  int dice_roof = atoi(argv[3]);
   if (dice_roof < dice_floor) { //Need better checks. Check if !NULL
     fprintf(stderr, "error: dice_roof must be at least = dice_floor\n");
     exit(-1);
@@ -41,8 +48,8 @@ int main(int argc, char* argv[]) {
   
   
   // check num_rolls
-  int num_rolls = atoi(argv[3]);
+  int num_rolls = atoi(argv[4]);
   if (num_rolls < 0) {    //Need better checks. Check if !NULL
-	  fprintf(stderr, "error: num_rolls must be >= 0");
+	  fprintf(stderr, "error: num_rolls must be >= 0\n");
 	  exit(-1);
   }
